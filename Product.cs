14768bed60efe9143f891385f9fbc56d0e75ef66
Ved Prakash <91253410+vp171212@users.noreply.github.com>
2023-08-11 22:05:45 +0530

    Add files via upload

---
+++ Product.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleProductApp.Model
+{
+    internal class Product
+    {
+        public int id;
+        public string name;
+        public int price;
+        public int discount;
+
+        public int DiscountedPrice()
+        { return price-price*discount/100; }
+    }
+}
